cb20cce48bd22b5ade814d184f772a2c2347b95d
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-03-15 14:11:23 -0600

    complete

---
+++ main.js
@@ -0,0 +1,24 @@
+const paragraph = document.querySelector('p');
+paragraph.style.color = "lightblue";
+
+const header = document.querySelector("h1");
+header.style.fontSize = "48px";
+
+const ps = document.querySelector('p');
+ps.innerHTML = "<p>Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.</p>"
+
+const item = document.querySelector("#item-13");
+item.style.opacity = ".5"
+
+const liItem3 = document.querySelector("#item-3");
+liItem3.innerHTML = "Hi!"
+
+const img = document.querySelector("img") 
+img.src = "http://www.tioxic.com/wp-content/uploads/trex_4.jpg"
+img.style.height = "300px"
+
+const img2 = document.querySelector("#img2");
+img.style.height = "300px";
+
+const liItem16 = document.querySelector("#item-16");
+liItem16.innerHTML = "Won't get fooled again."
